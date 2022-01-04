56aabaac4f987a4b9fca18ddb0242dce758b867a
Administrator <helloword.com>
2022-01-04 22:15:18 +0800

    Add build script

--- src/main/java/com/github/handler/OrangeInitializer.java
+++ src/main/java/com/github/handler/OrangeInitializer.java
@@ -6,6 +6,9 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.cors.CorsConfig;
+import io.netty.handler.codec.http.cors.CorsConfigBuilder;
+import io.netty.handler.codec.http.cors.CorsHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 
@@ -24,9 +27,9 @@ public class OrangeInitializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast(new HttpServerCodec());
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new ChunkedWriteHandler());
-
+        CorsConfig corsConfig = CorsConfigBuilder.forAnyOrigin().allowNullOrigin().allowCredentials().build();
+        pipeline.addLast(new CorsHandler(corsConfig));
         pipeline.addLast(new OrangeHandler(indexAccessor));
-        pipeline.addLast(new StaticServerHandler());
 
     }
 }
--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -21,7 +21,7 @@ function App() {
     }
 
     async function doTxtChange(v) {
-        let resp = await fetch(`http://localhost:3001/api/q?kw=${v}`);
+        let resp = await fetch(`http://localhost:8080/q?kw=${v}`);
         let json = await resp.json();
         setItems(json);
         setSuggestions(top6(json));
@@ -38,7 +38,7 @@ function App() {
 
     async function handleClick(toggleMenu) {
         toggleMenu();
-        let resp = await fetch(`http://localhost:3001/api/q?kw=${kw}`);
+        let resp = await fetch(`http://localhost:8080/q?kw=${kw}`);
         let json = await resp.json();
         setItems(json);
         setSuggestions(top6(json));
