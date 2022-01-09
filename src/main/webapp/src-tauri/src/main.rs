d03509755b3ba33dfdaf2dbd8953288b6fa318f7
Administrator <helloword.com>
2022-01-09 12:03:23 +0800

    Add tray support

--- src/main/webapp/src-tauri/src/main.rs
+++ src/main/webapp/src-tauri/src/main.rs
@@ -4,32 +4,39 @@ windows_subsystem = "windows"
 )]
 
 use std::process::Command;
-use std::thread;
-use tauri::{SystemTray};
+use std::{process, thread};
+use tauri::{Manager, SystemTray, SystemTrayEvent};
 use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::api::path::BaseDirectory::Runtime;
 
 fn main() {
-    let tray = SystemTray::new();
 
     thread::spawn(|| {
         Command::new("orange_core.exe")
             .output()
             .expect("failed to execute process");
-        println!("hhi")
     });
 
-    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu = SystemTrayMenu::new()
-        .add_item(quit)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(hide);
+        .add_item(quit);
     let tray = SystemTray::new().with_menu(tray_menu);
 
 
     tauri::Builder::default()
-        .system_tray(system_tray)
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+
+                match id.as_str() {
+                    "quit" => {
+                        process::exit(0x0100);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
